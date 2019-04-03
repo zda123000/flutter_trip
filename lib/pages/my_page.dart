9ee26c56be3ca9580a1c25f64bbf047f9c8d9ed4
zda <479146715@qq.com>
2019-04-03 15:46:39 +0800

    first commit

---
+++ lib/pages/my_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatefulWidget {
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  final PageController _controller = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('我的页面')));
+  }
+}
---
+++ lib/pages/travel_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TravelPage extends StatefulWidget {
+  _TravelPageState createState() => _TravelPageState();
+}
+
+class _TravelPageState extends State<TravelPage> {
+  final PageController _controller = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('旅拍')));
+  }
+}
